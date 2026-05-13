a0f7eb91e7d7d790bb66de33091dc87063c9a222
Emily Vavrinu <evavrinu@c1r6s5.42prague.com>
2026-05-13 17:07:26 +0200

    function that displays the number passed as a parameter. The function must be able to display all possible values of an int type variable.

---
+++ ex07/ft_putnbr.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: evavrinu <evavrinu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/13 16:16:52 by evavrinu          #+#    #+#             */
+/*   Updated: 2026/05/13 17:01:38 by evavrinu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+
+{
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		ft_putnbr(147483648);
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+		ft_putnbr(nb);
+	}
+	else if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+	{
+		ft_putchar(nb + '0');
+	}
+}
+// void	ft_putnbr(int nb);
+
+// int	main(void)
+// {
+// 	ft_putnbr(42);
+// 	write(1, "\n", 1);
+// 	ft_putnbr(0);
+// 	write(1, "\n", 1);
+// 	ft_putnbr(-123);
+// 	write(1, "\n", 1);
+// 	ft_putnbr(2147483647);
+// 	write(1, "\n", 1);
+// 	ft_putnbr(-2147483648);
+// 	return (0);
+// }
