691d063737d54285e2d2fbd66b4a2ca2d0ec68d3
Emily Vavrinu <evavrinu@c1r6s5.42prague.com>
2026-05-13 16:00:43 +0200

    function that displays all unique combinations of three distinct digits, with both the digits within each combination and the combinations themselves in ascending order.

---
+++ ex05/ft_print_comb.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: evavrinu <evavrinu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/13 15:34:32 by evavrinu          #+#    #+#             */
+/*   Updated: 2026/05/13 15:59:52 by evavrinu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_comb(void)
+{
+	int		a;
+	int		b;
+	int		c;
+	char	digit;
+
+	a = -1;
+	while (++a <= 7)
+	{
+		b = a;
+		while (++b <= 8)
+		{
+			c = b;
+			while (++c <= 9)
+			{
+				digit = a + '0';
+				write(1, &digit, 1);
+				digit = b + '0';
+				write(1, &digit, 1);
+				digit = c + '0';
+				write(1, &digit, 1);
+				if (!(a == 7 && b == 8 && c == 9))
+					write(1, ", ", 2);
+			}
+		}
+	}
+}
+
+void	ft_print_comb(void);
+
+int	main(void)
+{
+	ft_print_comb();
+	return (0);
+}
